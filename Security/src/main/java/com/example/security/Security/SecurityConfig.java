47dabd5c5b644faf2a26c603eb733474530fcbe5
Sarthak Lama <sarthaklama068@gmail.com>
2026-01-20 17:00:22 +0545

    Add Spring Security project

---
+++ Security/src/main/java/com/example/security/Security/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.security.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated());
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+}
