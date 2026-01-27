56e95251f289a15378e9fead1ae26fe630a8ac05
Sarthak Lama <sarthaklama068@gmail.com>
2026-01-27 17:52:11 +0545

    Password Bcrypt

--- Security/src/main/java/com/example/security/Security/SecurityConfig.java
+++ Security/src/main/java/com/example/security/Security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.security.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -9,13 +10,23 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Autowired
+    DataSource dataSource;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http){
         http.authorizeHttpRequests(authorizeRequests ->
@@ -33,21 +44,36 @@ public class SecurityConfig {
     public UserDetailsService userDetailsService(){
 
         UserDetails user1 = User.withUsername("User1")
-                .password("{noop}password1")
+//                .password("{noop}password1")
+                .password(passwordEncoder().encode("password1"))
                 .roles("USER")
                 .build();
 
         UserDetails user2 = User.withUsername("User2")
-                .password("{noop}password2")
+//                .password("{noop}password2")
+                .password(passwordEncoder().encode("password2"))
                 .roles("USER")
                 .build();
 
         UserDetails admin = User.withUsername("admin")
-                .password("{noop}adminPassword")
+//                .password("{noop}adminPassword")
+                .password(passwordEncoder().encode("adminPassword1"))
                 .roles("ADMIN")
                 .build();
 
-        return new InMemoryUserDetailsManager(user1,  user2, admin);
+//        return new InMemoryUserDetailsManager(user1,  user2, admin);
+
+        JdbcUserDetailsManager userDetailsManager =
+                new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.createUser(user1);
+        userDetailsManager.createUser(user2);
+        userDetailsManager.createUser(admin);
+        return userDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
